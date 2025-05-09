8e6d0d0178f64fc2e2b52ff091f43a89256db31f
frandavo <frandavowork@gmail.com>
2025-05-09 09:09:39 +0200

    feat: farmacia-local añadir proveedores y su css

---
+++ src/components/Proveedores.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import Navbar from './common/Navbar'; // Importar la barra de navegación
+import Footer from './common/Footer'; // Importar el componente de pie de página
+import PanelProveedores from './panels/PanelProveedores'; // Importar el componente de panel de producto
+import { SupplierContext } from "../context/ProveedoresContext"; // Importar las constantes
+import { TITULOS } from '../lib/constantes';
+const Proveedores = () => {
+    const { suppliers } = useContext(SupplierContext); // Usar el contexto para acceder a los productos
+
+    return (
+        <div className="productos-container">
+            <Navbar /> {/* Incluir la barra de navegación */}
+            <main className="productos-content">
+                <h1>{TITULOS.PANEL_DE_PROVEEDORES}</h1>
+                <div className="productos-paneles">
+                    {suppliers.map((supplier) => (
+                        <PanelProveedores key={supplier.id} supplier={supplier} />
+                    ))}
+                </div>
+            </main>
+            <Footer /> {/* Incluir el pie de página */}
+        </div>
+    );
+};
+
+export default Proveedores;
+
