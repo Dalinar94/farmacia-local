1f14ff3e5b5c051859b5ef0bf5f3d768bdbebcb7
frandavo <frandavowork@gmail.com>
2025-05-23 13:01:41 +0200

    feat:farmacia-local-back proveedores post

--- Backend/routes/proveedores.js
+++ Backend/routes/proveedores.js
@@ -8,4 +8,15 @@ router.get('/', async (req, res) => {
     res.json(proveedor);
 });
 
+// Ruta para agregar un proveedor
+router.post('/agregar', async (req, res) => {
+  try {
+    const nuevoProveedor = new Producto(req.body);
+    await nuevoProveedor.save();
+    res.status(201).json(nuevoProveedor);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al agregar el proveedor' });
+  }
+});
+
 module.exports = router;
